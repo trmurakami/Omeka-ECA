2170b8e0569537d6f345a95f45a5ae534c2c7052
Tiago Murakami <trmurakami@gmail.com>
2019-12-12 18:04:56 -0300

    Alterações no item

--- themes/denison/items/show.php
+++ themes/denison/items/show.php
@@ -42,13 +42,16 @@
   <article class="col-md-12">
     <div class="col-md-8 col-md-offset-2">
       <div class="article-content">
+      <!--
         <b>Descrição</b>
-        <p><?php echo $description; ?></p>
+        <p>< ?php echo $description; ?></p>
         <b>Fonte</b>
-        <p><?php echo $source; ?></p>
+        <p>< ?php echo $source; ?></p>
         <b>Tags</b>
-        <div id="tags-list"><?php echo $tags; ?></div>
-      </div><!-- end of article-content -->
+        <div id="tags-list">< ?php echo $tags; ?></div>
+      </div>
+      -->
+      <!-- end of article-content -->
     </div>
   </article>
       
@@ -65,7 +68,6 @@
     }
   
   ?>
-  
 
   <div class="item-description col-lg-12">
     <div class="col-lg-3 col-md-3 col-sm-6 col-lg-offset-2">
@@ -88,7 +90,62 @@
       ?>
     </div>
     
-  </div><!-- end of item-description -->
+  </div><!-- end of item-description -->  
+
+  <div class="flex">
+  <!-- The following returns all of the files associated with an item. -->
+
+    <div class="item-metadata">
+        <!--
+        <nav>
+          <ul class="item-pagination navigation">
+              <li id="previous-item" class="previous">< ?php echo link_to_previous_item_show(); ?></li>
+              <li id="next-item" class="next">< ?php echo link_to_next_item_show(); ?></li>
+          </ul>
+        </nav>
+        -->
+        
+        
+        <div class="item-metadata-content">      
+      
+          <?php if (metadata('item', 'Collection Name')): ?>
+          <div id="collection" class="element">
+              <h3><?php echo __('Collection'); ?></h3>
+              <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+          </div>
+          <?php endif; ?>
+        
+          <!-- The following prints a list of all tags associated with the item -->
+          <?php if (metadata('item', 'has tags')): ?>
+          <div id="item-tags" class="element">
+              <h3><?php echo __('Tags'); ?></h3>
+              <div class="element-text"><?php echo tag_string('item'); ?></div>
+          </div>
+          <?php endif;?>
+      
+          <!-- The following prints a citation for this item. -->
+          <div id="item-citation" class="element">
+              <h3><?php echo __('Citation'); ?></h3>
+              <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+          </div>
+        
+          <!--
+          <div id="item-output-formats" class="element">
+              <h3>< ?php echo __('Output Formats'); ?></h3>
+              <div class="element-text">< ?php echo output_format_list(); ?></div>
+          </div>
+          -->
+        
+          <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+        </div>
+    </div>
+  </div>  
+  
+
+
 </div><!-- end of item container -->
+
+
+
 
 <?php echo foot(); ?>
